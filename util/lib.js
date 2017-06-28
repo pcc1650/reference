b1cc2c0a1b84f3838830414ec64603bd19741a22
tonyzhao <tonyzhao1650@gmail.com>
2017-06-28 22:44:54 +0800

    add snake to camel

--- util/lib.js
+++ util/lib.js
@@ -1,3 +1,7 @@
 convertCamelToSnake = (camel) => {
     return camel.replace(/([A-Z])/g, function($1){return "_"+$1.toLowerCase();});
 }
+
+function snakeToCamel(s){
+    return s.replace(/(_\w)/g, function(m){return m[1].toUpperCase();});
+}
